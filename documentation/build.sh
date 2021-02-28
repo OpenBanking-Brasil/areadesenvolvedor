11c9e43d56dc80a1f37989a25d0c9e85ebac4c49
Elise <eeki@gft.com>
2021-02-28 00:00:32 -0300

    Add Unarranged Accounts Overdraft

--- documentation/build.sh
+++ documentation/build.sh
@@ -6,6 +6,7 @@ swagger-cli bundle source/swagger/parts/_accounts_apis_part.yml --outfile source
 swagger-cli bundle source/swagger/parts/_bank_borrowing_apis_part.yml --outfile source/swagger/swagger_bank_borrowing_apis.yaml --type=yaml
 swagger-cli bundle source/swagger/parts/_loans_apis_part.yml --outfile source/swagger/swagger_loans_apis.yaml --type=yaml
 swagger-cli bundle source/swagger/parts/_financings_apis_part.yml --outfile source/swagger/swagger_financings_apis.yaml --type=yaml
+swagger-cli bundle source/swagger/parts/_unarranged_accounts_overdraft_apis_part.yml --outfile source/swagger/swagger_unarranged_accounts_overdraft_apis.yaml --type=yaml
 swagger-cli bundle source/swagger/parts/_open_banking_apis_part.yml --outfile source/swagger/swagger_open_banking_apis.yml --type=yaml
 
 widdershins source/swagger/swagger_open_banking_apis.yml -o source/includes/partials_open_banking/_open_banking_apis.md.erb --user_templates source/templates/openapi3/ --language_tabs "javascript:JavaScript:request" "python:Python:request" "java:Java::request" --omitHeader --summary --httpsnippet
