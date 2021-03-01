f98217e9f15b8c842adaa7e9b31bba6aa36e5fdd
Luan Kevin Ferreira <lnfa@gft.com>
2021-03-01 13:46:17 -0300

    Add command to add BOM characters in swagger files.

--- documentation/build.sh
+++ documentation/build.sh
@@ -10,6 +10,8 @@ swagger-cli bundle source/swagger/parts/_unarranged_accounts_overdraft_apis_part
 swagger-cli bundle source/swagger/parts/_invoice_financings_apis_part.yml --outfile source/swagger/swagger_invoice_financings_apis.yaml --type=yaml
 swagger-cli bundle source/swagger/parts/_open_banking_apis_part.yml --outfile source/swagger/swagger_open_banking_apis.yml --type=yaml
 
+sed -i '1s/^\(\xef\xbb\xbf\)\?/\xef\xbb\xbf/' source/swagger/swagger_*
+
 widdershins source/swagger/swagger_open_banking_apis.yml -o source/includes/partials_open_banking/_open_banking_apis.md.erb --user_templates source/templates/openapi3/ --language_tabs "javascript:JavaScript:request" "python:Python:request" "java:Java::request" --omitHeader --summary --httpsnippet
 
 spectral lint source/swagger/*_apis.yaml
